a5b77ec2cef448d49754c07516d50466b19336c2
elouhichi <108538201+elouhichi@users.noreply.github.com>
2023-11-23 19:13:33 +0100

    first commit

---
+++ src/main/java/com/kata/BananesExport/BananesExportApplication.java
@@ -0,0 +1,13 @@
+package com.kata.BananesExport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BananesExportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BananesExportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kata/BananesExport/BananesExportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kata.BananesExport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BananesExportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
